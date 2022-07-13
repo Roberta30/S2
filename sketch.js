e37f527eb86dbc9b1f9ae85e674c953d876cace0
Roberta30 <107767893+Roberta30@users.noreply.github.com>
2022-07-13 16:27:27 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,64 +1,25 @@
-var path,boy,leftBoundary,rightBoundary;
-var pathImg,boyImg;
-var i;
+var marks = [35,38,42,45,43,34,46,41,48,32];
 
 
-function preload(){
-boyImg=loadAnimation("Runner-1.png","Runner-2.png","Runner-1.png","Runner-2.png");
+function setup() 
+{
+  createCanvas(400,400);
 
-pathImg = loadImage("path.png");
+  for(var c = 0; c < marks.length; c++){
+
+    if(marks[c] >= 35){
+      console.log(marks[c]);
+
+    }
+
+  }
+
 }
 
-function setup(){
-   createCanvas(400,400);
-    
- // Movendo o fundo 
-   path=createSprite(200,200);
-   path.addImage(pathImg);
-   path.velocityY = 4;
-   path.scale=1.2;
-
- //Criando menino que corre 
- //O MENINO NAO TA APARECENDO NA NOSSA TELA, A GENTE ACHA QUE TA CERTO, SO QUE NAO VAI, OBG S2
-   boy=createSprite(210,330,70,70);
-   boy.addAnimation("running", boyImg);
-   boy.scale= 1.;
-
-
- // Criando Boundary (Limite) esquerdo  
-   leftBoundary=createSprite(20,330,50,400);
-   leftBoundary.visible = false;
-
- //Crie Boundary direito 
-   rightBoundary=createSprite(385,330,50,400);
-   rightBoundary.visible = false;
+function draw() 
+{
+  background(30);
 }
 
-
-function draw() {
- background(0);
- path.velocityY = 4;
-
-
- // Menino se movendo no eixo X com o mouse
- boy.x = World.mouseX;
  
- edges= createEdgeSprites();
- boy.collide(edges[3]);
- boy.collide(leftBoundary);
- boy.collide(rightBoundary);
-
- //Reiniciar o fundo
- if(path.y > 444){
-   path.y = path.width;
- }
-
-
-
- drawSprites();
-
-}
-
-
-
 
